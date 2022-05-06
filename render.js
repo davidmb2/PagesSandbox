41843d73a0d797be037fc2040f85ba136991c1c7
davidmb2 <david.morales@wizeline.com>
2022-05-06 15:11:51 -0500

    Added document to render

--- render.js
+++ render.js
@@ -1,5 +1,6 @@
 (function () {
   var file = file || "README.md";
+  var file2 = file2 || "FirstDocument.md";
   var reader = new stmd.DocParser();
   var writer = new stmd.HtmlRenderer();
   var xhr = new XMLHttpRequest();
@@ -13,9 +14,9 @@
     var parsed = reader.parse(xhr.responseText);
     var content = writer.renderBlock(parsed);
     document.getElementsByTagName('body')[0].innerHTML = content;
-    
+
     /* try to extract h1 title and use as title for page
-       if no h1, use name of file 
+       if no h1, use name of file
     */
     try {
       document.title = document.querySelector('h1').textContent
@@ -25,5 +26,6 @@
   }
 
   xhr.open('GET', file);
+  xhr.open('GET', file2);
   xhr.send();
 })();
